870df22197cc44c2695b493b81265b465c291141
guifaxina <99680960+guifaxina@users.noreply.github.com>
2022-07-08 09:41:45 -0300

    Added: cors and final adjustments

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const apiRoute = require("./routes/api")
 app.use("/api", apiRoute)
 app.use("/", express.static(path.join(__dirname, "public")))
 
-
-
 app.listen(PORT, () => {
   console.log(`Servidor iniciado na porta ${PORT}`);
 });
---
+++ public/scripts/script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  render();
+});
+
+function render() {
+  fetch("http://192.168.1.2:3000/api/all")
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      let postElements = "";
+
+      let posts = JSON.parse(json);
+      posts.forEach((elem) => {
+        let postElement = 
+        ` <div id=${elem.id} class="card mb-4">
+            
+            <div class="card-header">
+              <h5 class="card-title">${elem.title}</h5>
+            </div>
+            
+            <div class="card-body">
+              <div class="card-text">${elem.description}</div>
+            </div>
+          
+          </div>`;
+        postElements += postElement;
+      });
+
+      document.getElementById("posts").innerHTML = postElements;
+    });
+}
+
+function newPost() {
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+
+  let post = {
+    title,
+    description,
+  };
+
+  const options = {
+    method: "POST",
+    headers: new Headers({ "content-type": "application/json" }),
+    body: JSON.stringify(post)
+  };
+
+  fetch("http://192.168.1.2:3000/api/new", options).then(res=>{
+    render();
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+  });
+}
